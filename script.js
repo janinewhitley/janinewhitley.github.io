1024efad493d689067c6d2ed60f8e816fb010426
janinewhitley <30781143+janinewhitley@users.noreply.github.com>
2021-06-11 11:15:43 -0400

    trying to fix menu closing issue

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.querySelectorAll('#menu li a').forEach(function(a) {
+    a.addEventListener('click', function() {
+      document.querySelector('#burger-toggle').checked = false;
+    });
+  });
 const loader = () => document.getElementById('loadscreen').style.display = 'none';
+  
