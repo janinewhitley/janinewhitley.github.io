f09e63348c9dd6adb4947f5fc8f0ff406a4ab7c9
janinewhitley <30781143+janinewhitley@users.noreply.github.com>
2021-12-31 21:12:46 -0500

    added typewriter effect

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const loader = () => {document.getElementById('loadscreen').style.display = 'none';}
 const closeHamburger = () => {document.getElementById('burger-toggle').checked = false;}
+
+var i = 0;
+var txt = "The name's Janine. Full-stack web developer. Here's some of my work.";
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.addEventListener('load', typeWriter);
